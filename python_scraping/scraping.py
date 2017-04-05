0ae444c98bad40aee2a0a6c1fb0ba63b98a7e6f0
whemygit <whemy@sina.com>
2017-04-05 22:55:53 +0800

    0405

---
+++ python_scraping/scraping.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -- coding: utf-8 --
+import sys
+
+reload(sys)
+sys.setdefaultencoding("utf-8")
+if __name__ == '__main__':
+    pass
+
+from urllib import *
+from bs4 import BeautifulSoup
+html=urlopen("http://www.pythonscraping.com/pages/warandpeace.html")
+bsObj=BeautifulSoup(html,'lxml')
+# print bsObj
+
+#根据标签属性查找所有标签
+# nameList=bsObj.find_all("span",{"class","green"})
+# print type(nameList)
+# for name in nameList:
+#     print name.get_text()
+
+#根据标签文本内容查找
+nameList=bsObj.find_all(text="the prince")
+# print type(nameList)
+# print len(nameList)
+# for name in nameList:
+#     print name
+
+
+
+
