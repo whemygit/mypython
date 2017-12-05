f43085482cb977cfeff23883db6ca54cbee5b728
whemygit <whemy@sina.com>
2017-12-05 14:41:25 +0800

    map_api

---
+++ R+python/map_api/baidu_api_test.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -- coding: utf-8 --
+import sys
+import json
+import requests
+
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+def getlnglat():
+    url='http://api.map.baidu.com/geocoder/v2/'
+    output='json'
+    ak='Qk77GopeGV7dPCPAAHpLHG0sl87pGbcV'
+    # add='北京市海淀区上地十街10号'
+    add = '伦敦'
+    uri=url+'?'+'address='+add+'&output=' + output + '&ak=' + ak
+    resp=requests.get(uri)
+    res=resp.text
+    temp=json.loads(res)
+    print type(temp)
+    print temp.keys()
+    print temp
+    print temp['result']['location']
+    print temp['result']
+
+
+if __name__ == '__main__':
+    getlnglat()
